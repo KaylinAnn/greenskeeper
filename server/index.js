f132776e1f240f4e07cfe1f638f22d9e7980d0a7
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-08-15 14:07:49 -0700

    added  mongo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const sensor = require('i2c-seesaw-moisture-sensor')
+const MongoClient = require("mongodb").MongoClient
+const ObjectId = require("mongodb").ObjectID
+require("dotenv").config();
+
+const app = express();
 
 app.use(bodyParser.json());
-
 
 let client = sensor.open(0x36)
 
@@ -22,6 +25,14 @@ client.getTemperature()
 
 app.get('/', (req, res) => {
   res.send('endpoint live')
+});
+
+const uri = process.env.CONNECTION_STRING
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("plants");
+
+  client.close();
 });
 
 const PORT = 4000;
