70fc0416ea33ba59a2164c2b68736f734660adb4
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-08-22 14:15:46 -0700

    added hourly temp avrg and a toggle for avrgs

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Dashboard() {
-  const [avrgTemp, setTemp] = useState('')
+  const [avrgDayTemp, setTemp] = useState('')
+  const [avrgHourTemp, setHrTemp] = useState('')
+  const [toggleTemps, setToggle] = useState(true)
+  var jsonDate = (new Date()).toJSON();
 
   useEffect(() => {
     getFullDayAvrgTemp()
+    getCurrHourAvrgTemp()
   })
 
   function getFullDayAvrgTemp() {
-    var jsonDate = (new Date('August 21, 2019 23:15:30 UTC')).toJSON();
-    console.log(jsonDate);
     let date = jsonDate.split('T')[0]
     let startDay = date + 'T00:00:00.000Z'
     let endDay = date + 'T23:59:59.999Z'
-    console.log(startDay, endDay);
 
     axios.post('/api/alltemps', { startDay, endDay })
 
@@ -25,11 +26,31 @@ function Dashboard() {
             return allTemps.push(temp.value)
           }
         })
-        console.log(allTemps);
         let sum = allTemps.reduce((acc, c) => acc + c, 0)
         let avrg = Math.round(sum / allTemps.length)
         setTemp(avrg)
-        console.log(avrg);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  function getCurrHourAvrgTemp() {
+    let date = jsonDate.split(':')[0]
+    let startDay = date + ':00:00.000Z'
+    let endDay = date + ':59:59.999Z'
+
+    axios.post('/api/alltemps', { startDay, endDay })
+      .then((res) => {
+        let allTemps = []
+        res.data.map(temp => {
+          if (temp.value < 125) {
+            return allTemps.push(temp.value)
+          }
+        })
+        let sum = allTemps.reduce((acc, c) => acc + c, 0)
+        let avrg = Math.round(sum / allTemps.length)
+        setHrTemp(avrg)
       })
       .catch(err => {
         console.log(err);
@@ -39,8 +60,9 @@ function Dashboard() {
   return (
     <div>
       <div>
-        {`Today's average Temp is ` + avrgTemp + 'F'}
+        {toggleTemps === true ? <div> {`Today's average Temp is ` + avrgDayTemp + 'F'}</div> : <div> {`Hourly average Temp is ` + avrgHourTemp + 'F'}</div>}
       </div>
+      <button onClick={toggleTemps === true ? () => setToggle(false) : () => setToggle(true)}>toggle</button>
     </div>
   )
 }
