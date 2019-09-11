72261627363f0c99643805ec157f0ad55cd76e76
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-09-11 12:14:51 -0700

    added admin signIn

--- src/components/adminAddPlant/adminAddPlant.js
+++ src/components/adminAddPlant/adminAddPlant.js
@@ -3,15 +3,39 @@ import './adminAddPlant.css'
 
 function AdminAddPlant() {
 
-  // const [] = useState()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  // useEffect(() => {
 
-  // })
+  function handleOnSubmit() {
+    if (username === '' && password === '') {
+      return alert('Please enter username and password.')
+    } else if (username === 'Admin' && password === 'password') {
+      console.log('SignedIn!');
+    } else {
+      return alert('Incorrect Username and Password.')
+    }
+
+  }
+
+
 
   return (
     <div className='addPlant-container'>
-      <div>Admin SignIn</div>
+      <div className='admin-title'>Admin SignIn</div>
+      <div className='username-container'>
+        <div className='username-title'>Username:</div>
+        <input placeholder='username' type="text" onChange={(e) => {
+          setUsername(e.target.value)
+        }} />
+      </div>
+      <div className='password-container'>
+        <div className='password-title'>Password:</div>
+        <input placeholder='password' type="text" onChange={(e) => {
+          setPassword(e.target.value)
+        }} />
+      </div>
+      <button className='submit-button' onClick={() => handleOnSubmit()}>Sign In</button>
     </div>
   )
 }
--- src/components/moisture/moisture.js
+++ src/components/moisture/moisture.js
@@ -26,17 +26,6 @@ function Moisture() {
           <div className='plant-name'>{plant.name}</div>
         </div>
       </div>
-      // <div className='moisture'>
-      //   <div className='icon-title-container'>
-      //     <img className='plant-icon' src={plant.img} alt="plant icon" />
-      //     <div className='plant-title-container'>
-      //       <div className='mapped-plants'>
-      //         |
-      //     </div>
-      //       <div className='plant-name'>{plant.name}</div>
-      //     </div>
-      //   </div>
-      // </div>
     )
   })
 
