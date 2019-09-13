27c2e80fd7c9cf42387c2ca116d8316f3f00aaf0
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-09-13 13:16:08 -0700

    added daily avg moisture

--- src/components/moisture/moisture.js
+++ src/components/moisture/moisture.js
@@ -7,6 +7,8 @@ function Moisture() {
 
   const [moisture, setMoisture] = useState([])
 
+  let jsonDate = (new Date()).toJSON()
+
   useEffect(() => {
     setMoistureState()
   })
@@ -14,9 +16,26 @@ function Moisture() {
   function setMoistureState() {
     Axios.get('/api/moisture')
       .then((res) => {
-        console.log(res.data[res.data.length + 1]);
+        let todaysMoistures = []
+        res.data.map(moisture => {
+          if (moisture.date.split('T')[0] === jsonDate.split('T')[0]) {
+            todaysMoistures.push(moisture.value)
+          }
+        })
+        let sum = todaysMoistures.reduce((acc, c) => acc + c, 0)
+        let avgMoisture = Math.round(sum / todaysMoistures.length)
+        console.log(avgMoisture);
 
+        setMoisture(avgMoisture)
       })
+  }
+
+  function moistureDialClassName() {
+    if (moisture > 600) {
+      return 'one'
+    } else if (moisture < 600 && moisture > 500) {
+      return 'two'
+    }
   }
 
   const plants = [{ name: 'Spider Plant', img: 'https://res.cloudinary.com/dclawygaw/image/upload/v1566857919/022-botanic.png' }, { name: 'Gold Dust', img: 'https://res.cloudinary.com/dclawygaw/image/upload/v1566857920/005-basil.png' }, { name: 'Cactus', img: 'https://res.cloudinary.com/dclawygaw/image/upload/v1566857920/011-botanic.png' }, { name: 'Monstara', img: 'https://res.cloudinary.com/dclawygaw/image/upload/v1566857921/033-botanic.png' }, { name: 'Peace Lily', img: 'https://res.cloudinary.com/dclawygaw/image/upload/v1566857920/004-anthurium.png' }]
